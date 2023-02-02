3981f6750401b9855f28856c7bf972dc85e36a34
Ethan Egerton <38867954+ethan-egerton@users.noreply.github.com>
2023-02-02 22:31:49 +0000

    Slimmed code, made more readable

--- pdfbatcher.py
+++ pdfbatcher.py
@@ -1,40 +1,38 @@
-import os
+from os import walk, path, makedirs
 from PIL import Image
-import time
+from time import time
 
-pdf_dir = "PDF"
-image_dir = "Images"
-if not os.path.exists(pdf_dir):
-    os.makedirs(pdf_dir)
-
-start_time = time.time()
+start_time = time()
 total_images = 0
 total_pdfs = 0
-for dirpath, dirnames, filenames in os.walk(image_dir):
-    if dirpath != image_dir and (any(f.endswith(".jpg") for f in filenames) or any(f.endswith(".png") for f in filenames) or any(f.endswith(".webp") for f in filenames)) and dirpath != pdf_dir:
-        images = [os.path.join(dirpath, f) for f in filenames if f.endswith(".jpg") or f.endswith(".png") or f.endswith(".webp")]
-        pdf_path = os.path.join(pdf_dir, os.path.basename(dirpath) + ".pdf")
-        im_list = []
-        total_images += len(images)
-        total_pdfs += 1
-        print(f"Processing folder: {dirpath}")
-        start_folder_time = time.time()
-        for i in images:
-            if i.endswith(".png"):
+pdf_dir = "PDF"
+image_dir = "Images"
+if not path.exists(pdf_dir):
+    makedirs(pdf_dir)
+
+for dirpath, dirnames, filenames in walk(image_dir):
+    try:
+        if dirpath != image_dir and dirpath != pdf_dir:
+            print(f"Processing folder: {dirpath}")
+            start_folder_time = time()
+            im_list = []
+            
+            pdf_path = path.join(pdf_dir, path.basename(dirpath) + ".pdf")
+            images = [path.join(dirpath, f) for f in filenames if f.endswith(".jpg") or f.endswith(".png") or f.endswith(".webp")]
+            total_images += len(images)
+            
+            for i in images:
                 img = Image.open(i).convert("RGB")
-                jpg_path = os.path.splitext(i)[0] + ".jpg"
+                jpg_path = path.splitext(i)[0] + ".jpg"
                 img.save(jpg_path)
                 im_list.append(img)
-            elif i.endswith(".webp"):
-                img = Image.open(i).convert("RGB")
-                jpg_path = os.path.splitext(i)[0] + ".jpg"
-                img.save(jpg_path)
-                im_list.append(img)
-            else:
-                im = Image.open(i)
-                im_list.append(im)
-        im_list[0].save(pdf_path, save_all=True, append_images=im_list[1:])
-        print(f"Time taken for folder {dirpath} <::> {time.time() - start_folder_time} seconds")
+
+            im_list[0].save(pdf_path, save_all=True, append_images=im_list[1:])
+            total_pdfs += 1
+            print(f"Time taken for folder {dirpath} <::> {time() - start_folder_time} seconds")
+    except:
+        print("Directory contained no images")
+
 print(f"Total images converted: {total_images}")
 print(f"Total PDFs created: {total_pdfs}")
-print(f"Total time taken: {time.time() - start_time} seconds")
+print(f"Total time taken: {time() - start_time} seconds")
